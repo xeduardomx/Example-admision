ba4d56631c1ad6a5d6e55188653b750b364e15ac
emen <eduardo.mendoza.288@gmail.com>
2019-08-31 12:35:10 -0300

    Se inicializa repositorio, se crea estructura de paquetes inicial y configuracion de propiedades

---
+++ src/test/java/com/syson/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syson.ejercicio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EjercicioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
