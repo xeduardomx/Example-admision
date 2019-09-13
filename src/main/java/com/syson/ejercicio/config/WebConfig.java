c38b19588d1b386d358845b255242344d62671cc
Eduardo Jose Mendoza Gutierrez <eduardo.mendoza.288@gmail.com>
2019-09-13 15:56:47 -0300

    Se cambia extends WebMvcConfigurerAdapter" la cual esta deprecada por "implements WebMvcConfigurer"

--- src/main/java/com/syson/ejercicio/config/WebConfig.java
+++ src/main/java/com/syson/ejercicio/config/WebConfig.java
@@ -3,13 +3,14 @@ package com.syson.ejercicio.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {//http://localhost:8080/api/swagger.json
 		registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/swagger-ui/dist/");
 	}
 }
+
